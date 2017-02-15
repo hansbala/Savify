7bda3e648bbc1b2e084b4355dffb9934e8c35337
Hans Bala <coolhansbala@gmail.com>
2017-02-15 15:47:25 +0530

    Add files via upload

---
+++ Linux/Setup_Linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+: '
+The following script is for Linux/UNIX only.
+This script installs the necessary packages 
+to get up and running.
+The following packages are installed and 
+upgraded-
+1. pip (Including upgrade)
+2. openpyxl (Used for reading the xlsx workbook)
+3. youtube-dl (For stripping the mp3 off YouTube)
+4. bs4 - BeautifulSoup(For clawing the webpage)
+'
+
+#Installing Pip and upgrading
+sudo apt-get install python-setuptools python-dev build-essential -y
+easy_install pip
+pip install -U pip
+
+#Installing openpyxl
+pip install openpyxl
+
+#Installing youtube-dl
+pip install youtube-dl
+
+#Installing BeautifulSoup
+pip install bs4
+
+clear
+echo "Successfully installed everything..."
+echo "Okay to exit and continue...."
+echo "You can close this window now."
